db429e34b5e838d69cc3a36bcedd3a4a417f01cc
Rik Huijzer <t.h.huijzer@rug.nl>
2022-09-08 14:54:26 +0200

    Remove Pirsch (#39)

--- src/ci.jl
+++ src/ci.jl
@@ -67,9 +67,5 @@ function build()
     write_thanks_page()
     fail_on_error = true
     gen(; fail_on_error)
-    extra_head = """
-        <script defer type="text/javascript" src="https://api.pirsch.io/pirsch.js"
-            id="pirschjs" data-code="fEU9oWaR1EuOsES86aD2guTf1qrDM4Ku"></script>
-        """
-    build_all(; extra_head, fail_on_error)
+    build_all(; fail_on_error)
 end
